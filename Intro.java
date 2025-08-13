c7b9add43c21bbdcdd2d3533ef7760e123cc525d
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-08-13 19:11:07 +0530

    basic program2

---
+++ Intro.java
@@ -0,0 +1,15 @@
+class Intro{
+	public static void main(String args[]){
+	
+	
+ 	System.out.println("name: sagar");
+	System.out.println("college: tit");
+	System.out.println("address: siwan");
+
+
+
+
+	}
+
+
+}
---
+++ Intro2.java
@@ -0,0 +1,10 @@
+class Intro2{
+	
+        public static void main(String args[]){
+	 System.out.println("name:sagar");
+	 System.out.println("college: tit");
+	 System.out.println("city:siwan");
+	 System.out.println("branch:cse");
+	}
+
+}
