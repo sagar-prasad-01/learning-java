88b9894fea64c496b100d2a456a4b7aa2b673da6
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-08-14 19:10:16 +0530

    sum program

---
+++ Addition.java
@@ -0,0 +1,14 @@
+//addition program
+class Addition{
+   public static void main(String args[]){
+	//declaire the variable
+	  int n1,n2,n3;
+	//inialize the variable
+	  n1=10;
+	  n2=20;
+	//perform operation
+	  n3=n1+n2;
+	 //print result
+	 System.out.println(n3);
+	}
+}
