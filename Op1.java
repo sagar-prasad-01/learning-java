1e1784959c4c5adff316b660c67a7eab88235eba
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-09-09 19:44:58 +0530

    logical operator

---
+++ Op1.java
@@ -0,0 +1,16 @@
+class Op1{
+public static void main(String ags[]){
+int a,b,c;
+a=100;
+b=200;
+c=300;
+Boolean d =a<b && b<c;
+Boolean e =a>b || b<c;
+System.out.println("ans="+d);
+System.out.println("ans="+e);
+System.out.println("ans="+!e);
+System.out.println("ans="+!!e);
+
+}
+
+}
