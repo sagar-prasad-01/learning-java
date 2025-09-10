a132a4eed3f5ae50c32d6a325ee733bc61b1c9d4
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-09-10 19:47:10 +0530

    bitwise operator

---
+++ Bool.java
@@ -0,0 +1,15 @@
+class Bool{
+	public static void main	(String args[]){
+	int a=6;
+	int b=7;
+	int c=a&b;
+	int d=a|b;
+	int e=a^b;//XOR operator
+	System.out.println("Bitwise and = "+c);
+        System.out.println("Bitwise or = "+d);
+	System.out.println("Bitwise xor = "+e);
+	
+		 
+
+}
+}
