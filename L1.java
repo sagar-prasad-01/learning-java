b2503be120f9f063538c71f7dd2031b231c6be82
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-09-26 19:20:13 +0530

    while loop

---
+++ L1.java
@@ -0,0 +1,17 @@
+//wap to print even num from 20 to 30
+class L1{
+	public static void main(String args[]){
+	int x=20;
+	while(x<=30){
+	if(x%2==0){
+	System.out.println("even number is "+x);
+
+	}
+	x++;
+
+	}
+	
+
+
+	}
+}
