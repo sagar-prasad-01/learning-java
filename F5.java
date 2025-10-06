1259e6cd65562061cca8173401a6884106e2e183
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-10-06 21:51:12 +0530

    loop

---
+++ F5.java
@@ -0,0 +1,16 @@
+//wap to print trianle pattern
+
+class F5{
+	public static void main(String args[]){
+	char A='A';
+
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=i;j++){
+		System.out.print(A);
+		
+		}
+		A++;
+	System.out.println("");
+	}
+
+}}
---
+++ F7.java
@@ -0,0 +1,15 @@
+//wap to print trianle pattern
+
+class F7{
+	public static void main(String args[]){
+	int n=1;
+	for(int i=1;i<=5;i++){
+	
+		for(int j=1;j<=i;j++){
+		System.out.print(n);
+		}
+		n++;
+	System.out.println("");
+	}
+
+}} 
