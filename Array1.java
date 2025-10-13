8fbcf0d6f303a032bd1ed1286754b9350a6674f9
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-10-13 23:16:56 +0530

    array

---
+++ Array1.java
@@ -0,0 +1,17 @@
+//array learning
+class Array1{
+	public static void main(String args[]){
+	//declairation of array(step 1)
+	int num[]=new int[5];
+	//initialization of array(step 2)
+	num[0]=10;
+	num[1]=20;
+	num[2]=40;
+	num[3]=30;
+	System.out.println(num[0]);
+	System.out.println(num[4]);
+
+	
+	//print elements of array(step 3)
+	}
+}
