9e32354f7fd23aff18c2759c8d17e7ea198f3668
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-11-05 19:41:07 +0530

    2D array

--- A21.java
+++ A21.java
@@ -22,7 +22,8 @@ class A21{
 	}
 
 }
-*/
+
+//
 class A21{
 	public static void main(String args[]){
 	int arr[][]=new int[][]{
@@ -37,3 +38,78 @@ class A21{
 	}
 
 }
+
+import java.util.Scanner;
+class A21{
+	public static void main(String args[]){
+	Scanner sk=new Scanner(System.in);
+	int arr[][]=new int[3][3];
+
+	System.out.println("enter elements index of 00");
+	arr[0][0]=sk.nextInt();
+	System.out.println("enter elements index of 01");
+	arr[0][1]=sk.nextInt();
+	System.out.println("enter elements index of 02");
+	arr[0][2]=sk.nextInt();
+	
+	System.out.println("enter elements index of 10");
+	arr[1][0]=sk.nextInt();
+	System.out.println("enter elements index of 11");
+	arr[1][1]=sk.nextInt();
+	System.out.println("enter elements index of 12");
+	arr[1][2]=sk.nextInt();
+
+	System.out.println("enter elements index of 20");
+	arr[2][0]=sk.nextInt();
+	System.out.println("enter elements index of 21");
+	arr[2][1]=sk.nextInt();
+	System.out.println("enter elements index of 22");
+	arr[2][2]=sk.nextInt();
+
+	
+	
+	System.out.println(arr[0][0]+"\t"+arr[0][1]+"\t"+arr[0][2]);
+	System.out.println(arr[1][0]+"\t"+arr[1][1]+"\t"+arr[1][2]);
+	System.out.println(arr[2][0]+"\t"+arr[2][1]+"\t"+arr[2][2]);
+	}
+
+}
+
+*/
+import java.util.Scanner;
+class A21{
+	public static void main(String args[]){
+	int arr[][]=new int[3][3];
+	Scanner sk=new Scanner(System.in);
+
+	for(int r=0;r<arr.length;r++){
+	for(int c=0;r<arr.length;c++){
+	System.out.println("enter elements index of"+r+c+" : ");
+	arr[r][c]=sk.nextInt();
+	}
+	System.out.println("");
+	}
+
+	for(int r=0;r<arr.length;r++){
+	for(int c=0;r<arr.length;c++){
+	System.out.print("\t"+arr[r][c]);
+	
+	}
+	System.out.println("");
+	}
+
+	for(int []a:arr){
+	for(int x:a){
+	System.out.print("\t"+x);
+	
+	}
+	System.out.println("");
+	}
+	
+	
+
+
+	}
+
+}
+
---
+++ A22.java
@@ -0,0 +1,50 @@
+//wap to print sum of two matrix
+import java.util.Scanner;
+class A22{
+	public static void main(String args[]){
+	int A[][]=new int[3][3];
+	int B[][]=new int[3][3];
+	int C[][]=new int[3][3];
+	int r,c;
+	Scanner sk=new Scanner(System.in);
+	
+	System.out.println("enter matrix A");
+	for( r=0;r<A.length;r++){
+	for( c=0;c<A.length;c++){
+	System.out.println("enter elements index of"+r+c+" : ");
+	A[r][c]=sk.nextInt();
+	}
+
+		}
+
+	
+	System.out.println("enter matrix B");
+	for( r=0;r<B.length;r++){
+	for( c=0;c<B.length;c++){
+	System.out.println("enter elements index of"+r+c+" : ");
+	B[r][c]=sk.nextInt();
+	}
+	
+	}
+
+	System.out.println("output of matrix A");
+	for(int[] r1:A){
+		for(int x:r1){
+		System.out.print("\t"+x);
+		}
+	System.out.println("");
+	}
+	
+	System.out.println("output of matrix B");
+	for(int[] r1:B){
+		for(int x:r1){
+		System.out.print("\t"+x);
+		}
+	System.out.println("");
+	}
+	
+	
+
+	}
+
+}
