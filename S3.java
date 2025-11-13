abeb9bf0c85fe067ccc458a84844c0f9dd4a6e36
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-11-13 22:41:36 +0530

    string

---
+++ S3.java
@@ -0,0 +1,38 @@
+/*
+class S3{
+	public static void main(String args[]){
+     	String x="sagar";
+	for(int i=x.length()-1;i>=0;i--){
+	char c=x.charAt(i);
+	System.out.print(c);
+	}
+	
+
+}}
+
+
+class S3{
+	public static void main(String args[]){
+     	String x="sag";
+	String x2="SAG";
+	String x3="G";
+	System.out.print(x.compareTo(x2));
+	System.out.print(x.compareTo(x3));
+	System.out.print(x.compareToIgnoreCase(x2));
+	
+	
+
+}}
+
+
+*/
+class S3{
+	public static void main(String args[]){
+     	String x="sag";
+	String y="ar";
+	
+	System.out.print(x.concat(y));
+	
+	
+
+}}
