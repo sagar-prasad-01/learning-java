8f29ee665e7c582763f42827cb3928614e356870
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-12-02 19:42:18 +0530

    access specifier

---
+++ AccessSpecifier.java
@@ -0,0 +1,41 @@
+//Default specifier
+/*
+class point{
+
+int x;
+int y;
+
+}
+
+class AccessSpecifier{
+
+public static void main(String args[]){
+
+point p1=new point();
+System.out.println(p1.x);
+System.out.println(p1.y);
+
+}
+
+}
+
+*/
+//private access specifier
+class point{
+
+private int x;
+private int y;
+
+}
+
+class AccessSpecifier{
+
+public static void main(String args[]){
+
+point p1=new point();
+System.out.println(p1.x);
+System.out.println(p1.y);
+
+}
+
+}
